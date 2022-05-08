f79da6bd04fc83165ad2d718e8228ea9a0b647de
adcommodore <alexisdcommodore@gmail.com>
2022-05-07 21:31:28 -0400

    done

--- more-forms/src/UserForm.jsx
+++ more-forms/src/UserForm.jsx
@@ -5,6 +5,7 @@ const UserForm = (props) => {
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     
     const createUser = (e) => {
         e.preventDefault();
@@ -15,6 +16,7 @@ const UserForm = (props) => {
         setLastName("");
         setEmail("");
         setPassword("");
+        setConfirmPassword("");
     };
     
     return(
@@ -23,38 +25,55 @@ const UserForm = (props) => {
                 <div>
                     <label>First Name: </label> 
                     <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+                    <br />
+                        { 
+                            firstName.length > 0 && firstName.length < 2 ?
+                            <span>First Name must be at least 2 characters.</span>
+                            : null
+                        }
                 </div>
                 <div>
                     <label>Last Name: </label> 
                     <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+                    <br />
+                        { 
+                            lastName.length > 0 && lastName.length < 2 ?
+                            <span>Last Name must be at least 2 characters.</span>
+                            : null
+                        }
                 </div>
                 <div>
                     <label>Email Address: </label> 
                     <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+                    <br />
+                        { 
+                            email.length > 0 && email.length < 5 ?
+                            <span>Email must be at least 5 characters.</span>
+                            : null
+                        }
                 </div>
                 <div>
                     <label>Password: </label>
                     <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+                    <br />
+                        { 
+                            email.length > 0 && email.length < 8 ?
+                            <span>Email must be at least 8 characters.</span>
+                            : null
+                        }
                 </div>
                 <div>
                     <label>Confirm Password: </label>
                     <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+                    <br />
+                        { 
+                            password !== confirmPassword ?
+                            <span>Passwords must match.</span>
+                            : null
+                        }
                 </div>
                     <input type="submit" value="Submit" />
             </form>
-            <div>
-                <p>Your Form Data</p>
-                <p>First Name {firstName} </p>
-                <p>Last Name {lastName} </p>
-                <p>Email {email}</p>
-                <p>Password {password}</p>
-                <p>Confirm Password {password}</p>
-            </div>
-            <div>
-              
-            </div>
-        
-
         </div>
     );
 };

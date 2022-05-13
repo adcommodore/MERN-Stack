1669f42075096960a7ee94719d1ce279de8f3399
adcommodore <alexisdcommodore@gmail.com>
2022-05-12 20:41:13 -0400

    done

--- jokes-api/server/config/mongoose.config.js
+++ jokes-api/server/config/mongoose.config.js
@@ -4,6 +4,6 @@ mongoose.connect('mongodb://localhost:27017/myfirstdatabase', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log('Established a connection to the database'))
-    .catch(err => console.log('Something went wrong when connecting to the database ', err));
+    .then(() => console.log('Connection establishedto the database'))
+    .catch(err => console.log('Error occured when connecting to the database ', err));
 
